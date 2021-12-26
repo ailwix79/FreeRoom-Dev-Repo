943906b056e490cc0c95aa3797c051d0f23cb4e0
ailwix79 <90765814+ailwix79@users.noreply.github.com>
2021-12-26 20:12:35 +0100

    Delete FreeRoomSERVER_mostRecent directory

--- FreeRoomSERVER_mostRecent/out/artifacts/FreeRoomSERVER_jar/repararJAR.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-zip -d *.jar 'META-INF/*.SF' 'META-INF/*.RSA' 'META-INF/*.DSA'
--- FreeRoomSERVER_mostRecent/target/classes/schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE tableCreator IF NOT EXISTS tableCreator (classId INT PRIMARY KEY, date DATE, time TIME, isFree BIT);
